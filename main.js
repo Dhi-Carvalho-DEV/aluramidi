2c7eaccae3272ac584e3a27802162189a6f7466d
Dhi-Carvalho-DEV <dhi.carvalho.dev@gmail.com>
2024-03-14 16:36:50 -0300

    Correção do código

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-function tocaSom(seletorAudio) {
+function tocaSom (seletorAudio) {
   const elemento = document.querySelector(seletorAudio);
 
-  if (elemento && elemento.localnome === 'audio') {
+  if (elemento && elemento.localName === 'audio') {
       elemento.play();
   }
   else {
-    console.log('Elemento não encontrado ou seletor inválido!');
+      //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado ou seletor inválido');
   }
+
 }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`;
+  const idAudio = `#som_${instrumento}`; //template string
 
   tecla.onclick = function () {
-    tocaSom(idAudio);
-  };
+      tocaSom(idAudio);
+  }
 
   tecla.onkeydown = function (evento) {
-    if(evento.code === "Enter" || evento.code === "Space") {
-      tecla.classList.add('ativa');
-    }
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+          tecla.classList.add('ativa');
+      }
   }
 
   tecla.onkeyup = function () {
-    tecla.classList.remove('ativa');
+      tecla.classList.remove('ativa');
   }
 }
